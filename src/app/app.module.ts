e94226083542fdf2395f5cc4af3f9442203e5957
Jeremias Zuber <jeremias.zuber@ict-campus.net>
2023-06-28 14:13:16 +0200

    Filenav color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FilenavComponent } from './filenav/filenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilenavComponent
   ],
   imports: [
     BrowserModule,
