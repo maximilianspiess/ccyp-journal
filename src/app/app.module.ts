631bb1ec36179172e95ce9980d8273c82371ea6e
Jeremias Zuber <jeremias.zuber@ict-campus.net>
2023-06-29 10:06:48 +0200

    Login structure started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FilenavComponent } from './filenav/filenav.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FilenavComponent
+    FilenavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
