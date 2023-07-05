234e2c7ad202a29d825795de3114f1a3189e920d
Maximilian Spiess <m.spiess.henning@gmail.com>
2023-07-05 17:08:55 +0200

    style buttons, textfields

--- src/app/fileopen/generated-question/generated-question.component.ts
+++ src/app/fileopen/generated-question/generated-question.component.ts
@@ -11,32 +11,28 @@ export class GeneratedQuestionComponent {
   @Input() genQuestion: string;
   @Output() genQuestionChange = new EventEmitter<string>();
   showComponent: boolean = true;
+  http: HttpClient;
+  data = null;
+  question = null;
 
+  ngOnInit() {
+    this.http.get<any>(this.url).subscribe(response => {
+      this.data = response;
+      this.question = this.data.question_text;
+    })
+  }
   onChange(event: any) {
     this.genQuestionChange.emit(event.target.value);
     // console.log("genQuestion.cp.ts, onChange: " + event.target.value);
   }
 
+  private url: string = "http://localhost:8080/getRandomQuestion"
 
-  // http: HttpClient;
-  // showComponent: boolean = true;
-  //
-  // data = null;
-  // question = null;
-  //
-  // private url: string = "http://localhost:8080/getRandomQuestion"
-  // ngOnInit() {
-  //   this.http.get<any>(this.url).subscribe(response => {
-  //     this.data = response;
-  //     this.question = this.data.question_text;
-  //   })
-  // }
-  //
-  // newQuestion(){
-  //   this.http.get<any>(this.url).subscribe(response => {
-  //     this.data = response;
-  //     this.question = this.data.question_text;
-  //   })
-  // }
+  newQuestion() {
+    this.http.get<any>(this.url).subscribe(response => {
+      this.data = response;
+      this.question = this.data.question_text;
+    })
+  }
 
 }
